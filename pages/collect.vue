7719c82448ee2e8c2e539234570d315b767e3d3e
葉奕緯 <aaa24295234@gmail.com>
2023-08-09 09:58:21 +0800

    fix map

--- pages/collect.vue
+++ pages/collect.vue
@@ -36,18 +36,18 @@
       </nuxt-link> -->
 
       <nuxt-link to="/collect" class="collect-box collect-box-1">
-        <img class="collect-img" src="@/assets/img/collect/qinai.png" alt="btn">2
+        <img class="collect-img" src="@/assets/img/collect/qinai.png" alt="btn">0
       </nuxt-link>
       <nuxt-link to="/collect" class="collect-box collect-box-2">
-        <img class="collect-img collect-img-special" src="@/assets/img/collect/anlei.png" alt="btn">1
+        <img class="collect-img collect-img-special" src="@/assets/img/collect/anlei.png" alt="btn">0
       </nuxt-link>
       <nuxt-link to="/collect" class="collect-box collect-box-3">
         <img class="collect-img" src="@/assets/img/collect/sinyi.png" alt="btn">0
       </nuxt-link>
       <nuxt-link to="/collect" class="collect-box collect-box-4">
-        <img class="collect-img" src="@/assets/img/collect/chunchen.png" alt="btn">1
+        <img class="collect-img" src="@/assets/img/collect/chunchen.png" alt="btn">0
       </nuxt-link>
-      <nuxt-link to="/collect" class="collect-box collect-box-5">
+      <nuxt-link to="/map-chunshan" class="collect-box collect-box-5">
         <img class="collect-img" src="@/assets/img/collect/chunshan.png" alt="btn">1
       </nuxt-link>
       <nuxt-link to="/collect" class="collect-box collect-box-6">
--- pages/map-chunshan.vue
+++ pages/map-chunshan.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="map"
-    :style="{
-      'backgroundImage' : 'url(' + require('@/assets/img/index/bg3.png') + ')'
-    }"
+  :style="{
+    'backgroundImage' : 'url(' + require('@/assets/img/index/bg3.png') + ')'
+  }"
   >
     <div class="map-row0">
       <img class="map-img" src="@/assets/img/collect/chunshan.png" alt="fb">
@@ -11,47 +11,19 @@
       </nuxt-link>
     </div>
 
-    <div class="map-outer">
-      <div class="map-left">
-        <div class="map-box">
-          <div class="map-row">
-            <div>吳XX</div>
-            <div class="map-want">想找</div>
-            <div>建築/場所</div>
-          </div>
-          <div class="map-text">章魚游泳池</div>
-        </div>
-      </div>
-      <div class="map-right">
-
-        <div class="map-box2">
-          <div class="map-row2">
-            <div>
-              <div class="map-name">吳XX</div>
-              <div class="map-row3">
-                <div class="map-want2">想找</div>
-                <div>建築/場所</div>
-              </div>
-            </div>
-            <div class="map-like">
-              <div class="fb-like" data-href="https://frankyeah.github.io/disappear/anlei?id=吳芷儀" data-width="300" data-layout="" data-action="" data-size="" data-share="true"></div>
-            </div>
-          </div>
-          <div class="map-text2">以前章魚游泳池也是很適合全家一起游泳的場所，孩子可以玩水、附近也有許多熱鬧的攤家、也有完善的設施，但現在只剩下海興以及沙灘，有點太過開放的場域，對於大家要鄰近海邊玩水的感覺比較困難～～</div>
-          <div class="map-title">我提供的線索</div>
-          <div class="map-text3">在大約國小前，應該80年？有個很大的章魚造景，記得好像還有滑水道？池子很大，大人小孩都能一起玩，也很乾淨！</div>
-        </div>
-        
-        <div class="map-fb">
-          <div class="fb-comments" data-href="https://frankyeah.github.io/disappear/anlei?id=吳芷儀" data-width="300" data-colorscheme="light" data-numposts="5"></div>
-        </div>
-      </div>
+    <div class="map-box">
+      <a class="map-item"
+        v-for="(item, index) in items"
+        :key="index"
+        :href="item.link"
+        target="_blank"
+      >
+        <div class="map-text1">{{ item.name }} 想找 {{ item.class }}</div>
+        <div class="map-text2">{{ item.text }}</div>
+      </a>
     </div>
 
-    <!-- <div class="fb-comments map-fb" data-href="https://siddharam.com/anlei" data-width="300" data-colorscheme="light" data-numposts="5"></div> -->
-
-
-</div>
+  </div>
 </template>
 
 <script>
@@ -68,28 +40,13 @@ export default {
   },
   data () {
     return {
-      isFBReady: false,
-      isFromIndexPage: false
+      items: [
+        { name: '吳小姐', class: '場所', text: '章魚游泳池', link: 'https://www.facebook.com/100088446610563/posts/pfbid0362nhTnJMLERWLfVTVUBAAzCg42trcp79vBBoni2gpk7PpYTVWfv5CMceJ4dFtiFhl/?mibextid=cr9u03' },
+      ]
     }
-  },
-    beforeRouteEnter(to, from, next) {
-      next(vm => {
-      // 判斷是否從首頁過來
-      if(from.name) {
-      console.log(from.name.substr(0, 5))
-        if(from.name.substr(0, 8) == 'collect') {
-          console.log(from.name.substr(0, 8))
-          console.log(from.name)
-          vm.isFromIndexPage = true
-        }
-      }
-      vm.prevRoute = from
-    })
   },
   mounted () {
-    if(this.isFromIndexPage) {
-      location.reload()
-    }
+    
     // this.isFBReady = Vue.FB != undefined
     // window.addEventListener('fb-sdk-ready', this.onFBReady)
   },
